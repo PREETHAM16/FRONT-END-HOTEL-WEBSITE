c54abea61def3a3e413403eea31220c9e29a5c00
PREETHAM16 <63236509+PREETHAM16@users.noreply.github.com>
2020-07-17 04:18:41 -0700

    Add files via upload

---
+++ frontend of hotel/index.js
@@ -0,0 +1,13 @@
+setTimeout(function(){
+document.getElementById("a").style.display = "block";
+document.getElementById("b").style.display = "block";
+document.getElementById("c").style.display = "none";
+  document.body.style.backgroundImage="url('bgimg.jpg')";
+}, 1000);
+function clr(){
+  document.getElementById('onc').style.display='none';
+}
+function disp(){
+  window.scrollTo(0,0);
+    document.getElementById('onc').style.display='block';
+}
